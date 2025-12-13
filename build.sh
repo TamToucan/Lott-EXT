98a62742060cbd1436ac675e45484cb007e3bee9
Tam Toucan <dr@tamtoucan.com>
2025-12-13 16:55:47 +0000

    Allow core/godot to be built

--- build.sh
+++ build.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-cmake --preset=mingw64 -S . -B build
-cmake --build build
+opt=""
+target=""
+
+[[ -z "$1" ]] && echo "$0 <core | godot | all>" && exit 1
+
+[[ $1 == "core" ]] &&
+opt="-DBUILD_GODOT=OFF" && target="core"
+
+[[ $1 == "godot" ]] &&
+opt="-DBUILD_GODOT=ON" && target="godot"
+
+[[ $1 == "all" ]] &&
+opt="-DBUILD_GODOT=ON" && target=""
+
+cmake --preset=mingw64 -S . -B build $opt
+cmake --build build --target $target
 
